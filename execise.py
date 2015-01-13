5dce772f5c0ebb2e4ce751de6ad92b6a935f99fb
Alledged <afubafu@gmail.com>
2015-01-13 09:37:17 +0100

    added the execise.py file

---
+++ execise.py
@@ -0,0 +1,7 @@
+# random file for python fun
+from CGAL.CGAL_Kernel import Point_2
+from CGAL.CGAL_Triangulation_2 import Triangulation_2
+from CGAL.CGAL_Triangulation_2 import Triangulation_2_Vertex_circulator
+from CGAL.CGAL_Triangulation_2 import Triangulation_2_Vertex_handle
+
+
